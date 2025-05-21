32c73b28479a82866e65b4bdb9e99646da97802a
harinibaskar <hbaskar@calpoly.edu>
2025-05-20 22:54:37 -0700

    "message"

--- src/main/java/PongPanel.java
+++ src/main/java/PongPanel.java
@@ -1,13 +1,6 @@
-
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-
-
-
-
-
 
 /**
  * This class is a simple JPanel that will be used to display the Bars, Ball, and Chat.
@@ -15,86 +8,57 @@ import java.awt.event.*;
  * @author hbaskar
  * @version 1.1
  */
+public class PongPanel extends JPanel implements MouseMotionListener, MouseListener, ActionListener {
 
-
-
-
-
-public class PongPanel extends JPanel implements MouseMotionListener, MouseListener {
-
-
-        public PongPanel() {
+    public PongPanel() {
         addMouseListener(this);
         addMouseMotionListener(this);
-        // Start a timer for ball movement every 50ms
+        // Start a timer for any future updates (if needed)
         Timer timer = new Timer(50, this);
         timer.start();
     }
 
-
-
-  
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        drawCenterLine(g);
-
-
-
-    }
-
-
-
-    private void drawCenterLine(Graphics g) {
         g.setColor(Color.WHITE);
-        Graphics2D g2d = (Graphics2D) g;
-        float[] dash1 = {10.0f};
-        BasicStroke dashed = new BasicStroke(
-                1.0f,
-                BasicStroke.CAP_BUTT,
-                BasicStroke.JOIN_MITER,
-                10.0f,
-                dash1,
-                0.0f);
-        g2d.setStroke(dashed);
-        g.drawLine(400, 0, 400, 600);
-        g2d.setStroke(new BasicStroke());
+        g.fillRect(0, 0, getWidth(), getHeight());
     }
-
-
-
 
     @Override
     public Dimension getPreferredSize() {
         return new Dimension(800, 600);
     }
 
+    // Empty implementations for MouseMotionListener
     @Override public void mouseDragged(MouseEvent e) {}
+    @Override public void mouseMoved(MouseEvent e) {}
+
+    // Empty implementations for MouseListener except mouseClicked
     @Override public void mousePressed(MouseEvent e) {}
     @Override public void mouseReleased(MouseEvent e) {}
     @Override public void mouseEntered(MouseEvent e) {}
     @Override public void mouseExited(MouseEvent e) {}
 
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        int mouseX = e.getX();
+        int mouseY = e.getY();
 
+        System.out.println("Screen clicked at: " + mouseX + ", " + mouseY);
 
+        // Store coordinates in Repository singleton
+        Repository.getInstance().setCoordinates(mouseX, mouseY);
 
+        // Publish coordinates via MQTT publisher class
+        MqttPublisher.publishCoordinates(mouseX, mouseY);
+    }
 
-
-@Override
-public void mouseClicked(MouseEvent e) {
-    int mouseX = e.getX();
-    int mouseY = e.getY();
-
-    System.out.println("Screen clicked at: " + mouseX + ", " + mouseY);
-
-    // Store coordinates
-    Repository.getInstance().setCoordinates(mouseX, mouseY);
-
-    // Publish coordinates to MQTT
-    MqttPublisher.publishCoordinates(mouseX, mouseY);
+    // Timer callback (required by ActionListener)
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // If needed, update the panel or repaint here
+        // For now, just repaint
+        repaint();
+    }
 }
-
-} 
-
-
-
