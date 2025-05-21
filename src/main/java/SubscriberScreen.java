c126a2ee65fe5d9965c861449058ce334546fdf2
Darien Rodrigo <darien.rodrigo05@gmail.com>
2025-05-20 22:28:10 -0700

    mouse implement

--- src/main/java/SubscriberScreen.java
+++ src/main/java/SubscriberScreen.java
@@ -1,6 +1,8 @@
 
 import javax.swing.*;
-import  java.awt.*;
+import java.awt.*;
+import java.awt.event.*;
+import org.w3c.dom.events.MouseEvent;
 
 /**
  * A simple GUI panel that draws a circle based on coordinates
@@ -9,7 +11,7 @@ import  java.awt.*;
  * Author: Darien Rodrigo
  */
 
-public class SubscriberScreen extends JPanel {
+public class SubscriberScreen extends JPanel implements ActionListener, MouseMotionListener, MouseListener {
     public SubscriberScreen(){
         setBackground(new Color(255,255,255));
     }
@@ -40,4 +42,56 @@ public class SubscriberScreen extends JPanel {
         frame.add(screen);
         frame.setVisible(true);
     }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+        if (Repository.getInstance().getWhoAmI() == Repository.CLIENT)
+            Repository.getInstance().setClientPlayerY(e.getY());
+        else
+            Repository.getInstance().setServerPlayerY(e.getY());
+        repaint();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Repository.getInstance().moveBall();
+        repaint();
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        int mouseX = e.getX();
+        int mouseY = e.getY();
+        int ballX = Repository.getInstance().getBallX();
+        int ballY = Repository.getInstance().getBallY();
+
+        if (mouseX >= ballX - 5 && mouseX <= ballX + 5 &&
+                mouseY >= ballY - 5 && mouseY <= ballY + 5) {
+            System.out.println("Ball clicked at: " + mouseX + ", " + mouseY);
+            Repository.getInstance().setCoordinates(mouseX, mouseY);
+        }
+    }
+
+
+    @Override
+    public void mousePressed(java.awt.event.MouseEvent e) {
+    }
+
+    @Override
+    public void mouseReleased(java.awt.event.MouseEvent e) {
+    }
+
+    @Override
+    public void mouseEntered(java.awt.event.MouseEvent e) {
+    }
+
+    @Override
+    public void mouseExited(java.awt.event.MouseEvent e) {
+    }
+
+    @Override
+    public void mouseDragged(java.awt.event.MouseEvent e) {
+    }
+
+    
 }
